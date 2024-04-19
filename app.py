8c216d1135ed2654f28f50528be0e2642e935e2a
Aryan Poonacha <poonacharyan@gmail.com>
2024-04-19 04:03:49 -0400

    catch up to deployment repo checkpoint

--- app.py
+++ app.py
@@ -42,13 +42,11 @@ def chat_with_model(prompt):
     )
     return response.json()[0]["generated_text"]
 
-
 st.session_state["corpus_number"] = st.secrets["VECTARA_CORPUS_ID"]
 st.session_state["vectara_api_key"] = st.secrets["VECTARA_API_KEY"]
 st.session_state["vectara_customer_id"] = st.secrets["VECTARA_CUSTOMER_ID"]
 st.session_state["hf_model_id"] = st.secrets["HF_MODEL_ID"]
 st.session_state["hf_token"] = st.secrets["HF_TOKEN"]
-
 
 # Streamlit page configuration
 st.set_page_config(page_title="Duke AI Chatbot", page_icon="💬")
@@ -59,7 +57,7 @@ st.title("Duke AI Chatbot")
 st.markdown("Welcome to the Duke AI chatbot. I can answer any of your questions about the Duke M.Eng. AI program!")
 st.markdown("By Aryan Poonacha for AIPI 590 - Spring '24")
 url = 'https://duke.qualtrics.com/jfe/form/SV_9ZAdIFH74vz6O46'
-st.markdown("[Provide Feedback Here!](%s)" % url)
+st.markdown("Provide Feedback Here!" % url)
 
 # Chat message handling
 if "messages" not in st.session_state:
@@ -69,14 +67,9 @@ with st.form("chat_input", clear_on_submit=True):
     user_prompt = st.text_input("Your message:", label_visibility="collapsed")
     if st.form_submit_button("Send"):
         st.session_state.messages.append({"role": "user", "content": user_prompt})
-
-if st.form_submit_button("Send"):
-    st.session_state.messages.append({"role": "user", "content": user_prompt})
-    model_response = chat_with_model(user_prompt)
-    st.session_state.messages.append({"role": "assistant", "content": model_response})
+        model_response = chat_with_model(user_prompt)
+        st.session_state.messages.append({"role": "assistant", "content": model_response})
 
 # Display chat messages
 for idx, msg in enumerate(st.session_state.messages):
     message(msg["content"], is_user=msg["role"] == "user", key=f"chat_message_{idx}")
-
-    
